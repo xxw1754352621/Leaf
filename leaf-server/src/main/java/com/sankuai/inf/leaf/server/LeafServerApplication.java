b3d44a5a9e6eaa095ab491656b614704a60ec8f8
kaka <1754352621@qq.com>
2020-01-21 16:39:54 +0800

    author：kaka desc：添加druid监控数据源，和过滤器，考虑切换其他数据源

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafServerApplication.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafServerApplication.java
@@ -1,12 +1,19 @@
 package com.sankuai.inf.leaf.server;
 
+import com.sankuai.inf.leaf.server.druid.stat.DruidStatViewServletConfiguration;
+import com.sankuai.inf.leaf.server.druid.stat.DruidWebStatFilterConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({
+        DruidStatViewServletConfiguration.class,
+        DruidWebStatFilterConfiguration.class
+})
 public class LeafServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LeafServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LeafServerApplication.class, args);
+    }
 }
